b8998ba5f474b1b24dfd7a7e7877f1489d92e8aa
Diego Mercado <diego.mercado@black-tobacco.com>
2016-09-01 16:28:22 -0300

    Added LoginActivity and NewsActivity

---
+++ app/src/main/java/famaf/unc/edu/ar/activitiesassignment/NewsActivity.java
@@ -0,0 +1,45 @@
+package famaf.unc.edu.ar.activitiesassignment;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class NewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_news, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_sign_in) {
+            NewsActivityFragment newsfragment = (NewsActivityFragment)
+                    getSupportFragmentManager().findFragmentById(R.id.news_activity_fragment_id);
+            TextView textView = (TextView) findViewById(R.id.loginStatusTextView);
+            textView.setText("User XXXX logged in");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
