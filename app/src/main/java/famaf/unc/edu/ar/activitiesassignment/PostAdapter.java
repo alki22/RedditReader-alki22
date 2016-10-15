a0ea5846cc23d459681349c077bbabcd43c1d00c
alki22 <alki.collias@gmail.com>
2016-10-15 20:11:38 -0300

    Implemented AsyncTask image downloader

--- app/src/main/java/famaf/unc/edu/ar/activitiesassignment/PostAdapter.java
+++ app/src/main/java/famaf/unc/edu/ar/activitiesassignment/PostAdapter.java
@@ -1,20 +1,47 @@
 package famaf.unc.edu.ar.activitiesassignment;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
-
 
 public class PostAdapter extends ArrayAdapter<PostModel> {
 
     private List<PostModel> PMList;
     int resourceNo;
+
+    class ImageDownloader extends AsyncTask<URL, Integer, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(URL ... urls) {
+            URL url = urls[0];
+            Bitmap bitmap = null;
+            HttpURLConnection connection = null;
+
+            try{
+                connection = (HttpURLConnection)url.openConnection();
+                InputStream stream = connection.getInputStream();
+                bitmap = BitmapFactory.decodeStream(stream, null, null);
+            } catch(IOException exception) {
+                exception.printStackTrace();
+            }
+
+            return bitmap;
+        }
+    }
 
     static class ViewHolder {
         TextView title;
@@ -22,7 +49,7 @@ public class PostAdapter extends ArrayAdapter<PostModel> {
         TextView postDate;
         TextView commentsNumber;
         ImageView thumbnail;
-        int position;
+        ProgressBar bar;
     }
 
     public PostAdapter(Context context, int resource, List<PostModel> list) {
@@ -54,7 +81,7 @@ public class PostAdapter extends ArrayAdapter<PostModel> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        ViewHolder holder;
+        final ViewHolder holder;
         if (convertView == null) {
             LayoutInflater vi = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = vi.inflate(resourceNo, parent, false);
@@ -64,6 +91,7 @@ public class PostAdapter extends ArrayAdapter<PostModel> {
             holder.subreddit = (TextView) convertView.findViewById(R.id.subreddit);
             holder.postDate = (TextView) convertView.findViewById(R.id.post_date);
             holder.thumbnail = (ImageView) convertView.findViewById(R.id.thumbnail);
+            holder.bar = (ProgressBar) convertView.findViewById(R.id.progress_bar);
             convertView.setTag(holder);
         }
         else {
@@ -72,12 +100,33 @@ public class PostAdapter extends ArrayAdapter<PostModel> {
 
         PostModel post = PMList.get(position);
 
+        URL[] urls = urlArrayCreate(post);
+        new ImageDownloader(){
+            @Override
+            protected void onPostExecute(Bitmap result) {
+                super.onPostExecute(result);
+                holder.bar.setIndeterminate(true);
+                holder.bar.setVisibility(View.GONE);
+                holder.thumbnail.setImageBitmap(result);
+                holder.thumbnail.setVisibility(View.VISIBLE);
+            }
+        }.execute(urls);
         holder.title.setText(post.getmTitle());
         holder.commentsNumber.setText(String.valueOf(post.getmCommentsNumber()));
         holder.subreddit.setText(post.getmSubreddit());
         holder.postDate.setText(post.getmPostDate());
-        holder.thumbnail.setImageResource(post.getmThumbnail());
 
         return convertView;
+    }
+
+    private URL[] urlArrayCreate(PostModel post) {
+        URL[] urls = new URL[1];
+        try {
+            urls[0] = new URL(post.getmThumbnail());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return urls;
     }
 }
