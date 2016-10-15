446b3a6f6253b154c8a97ae5a3d91386256cf881
alki22 <alki.collias@gmail.com>
2016-10-15 20:11:09 -0300

    Replaced harcoded images for URLs

--- app/src/main/java/famaf/unc/edu/ar/activitiesassignment/Backend.java
+++ app/src/main/java/famaf/unc/edu/ar/activitiesassignment/Backend.java
@@ -15,16 +15,26 @@ public class Backend {
 
         topPosts = new ArrayList<>();
 
-        PostModel pm1 = new PostModel("Durísimo golpe: China suspende la compra de soja al país",
-                "7h", "/r/Argentina", 114, R.drawable.pm1);
-        PostModel pm2 = new PostModel("Miami, never change.", "27m", "/r/Miami", 2,
-                R.drawable.pm2);
-        PostModel pm3 = new PostModel("Miami/Dade be like", "4d", "/r/Miami", 543, R.drawable.pm3);
+        PostModel pm1 = new PostModel(
+                "Venecia es para chetos, pegense una vuelta por mi barrio, no se arrepentirán :)",
+                "7h", "/r/Argentina", 114,
+                "https://b.thumbs.redditmedia.com/pxpYCHL7wwsTAFf0vtSxDhI7MdN3Z45cERS02oPgUII.jpg");
+        PostModel pm2 = new PostModel(
+                "Any Miami cyclists love riding to catch a good view? ",
+                "27m", "/r/Miami", 2,
+                "https://b.thumbs.redditmedia.com/l1Z9YW2kZ5wDV1A4ePlbTRnmivgpU79D0re9GQ-puLo.jpg");
+        PostModel pm3 = new PostModel(
+                "The greatest man of all time",
+                "4d", "/r/Miami", 543,
+                "https://b.thumbs.redditmedia.com/VRkYXcGRw2-5zLLU.jpg");
         PostModel pm4 = new PostModel
-                ("Cáncer en Córdoba: las zonas intensamente agricólas tienen mortalidad más alta",
-                        "1d", "/r/Argentina", 444, R.drawable.pm4);
-        PostModel pm5 = new PostModel("Este Domingo Campeonato de Asado en la 9 de Julio.",
-                "1m", "/r/Argentina", 22, R.drawable.pm5);
+                ("El caso de Martina en el tribunal intetnacional contra Monsanto. ", "1d",
+                        "/r/Argentina", 444,
+                        "https://b.thumbs.redditmedia.com/PVSFRc_O9SmvfAJ-mSf5YM9P-9jelU5TROaHJyvBJHU.jpg");
+        PostModel pm5 = new PostModel(
+                "Visto en la villa 31",
+                "1m", "/r/Argentina", 22,
+                "https://b.thumbs.redditmedia.com/ojRFshQ7r-HhD25fD6bJENRIHStOOXx_vZtGQLyShZs.jpg");
 
         topPosts.add(pm1);
         topPosts.add(pm2);
